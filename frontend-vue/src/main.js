34c36c33d69587baf869a15b6c5c8727ed38c3f2
wuenzeboss <alex0132@163.com>
2022-05-06 11:27:04 +0800

    ADD: 添加Element-UI库

--- frontend-vue/src/main.js
+++ frontend-vue/src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 new Vue({
+  el: '#app',
   render: h => h(App),
   router,
 }).$mount('#app')
