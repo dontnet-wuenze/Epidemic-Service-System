738ef0a90dcc0ede3b547be671054679c03297cd
wuenzeboss <alex0132@163.com>
2022-05-12 17:26:39 +0800

    ADD: 添加主页

---
+++ frontend-vue/src/views/health/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="indexContainer">
+    <el-row type="flex" class="row-bg" gutter="30">
+      <el-col :span="3"><button class="grid-content" @click="jump('/health/attend')">健康打卡<img src="/favicon.ico"></button></el-col>
+      <el-col :span="3"><button class="grid-content" @click="jump('/nucleic/result')">打卡统计</button></el-col>
+      <el-col :span="3"><button class="grid-content" @click="jump('/health/passcode')">查看通行码</button></el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  methods: {
+    jump(path) {
+      this.$router.push(path)
+    }
+  }
+}
+</script>
+<style scoped src="../../../public/css/index.css"></style>
