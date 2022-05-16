a3fa79946f50c7089e6c6aa254f1e41b75ff7462
wuenzeboss <alex0132@163.com>
2022-05-16 17:30:24 +0800

    UPD: 修改header样式

--- frontend-vue/src/views/heaader/header.vue
+++ frontend-vue/src/views/heaader/header.vue
@@ -1,13 +1,61 @@
 <template>
-  <el-container style="min-width: 1200px">
+  <el-container style="min-width: 1400px">
     <el-header>
-      <el-row>
-        <el-col :span="4" :offset="3">浙江大学疫情服务与管理系统</el-col>
-        <el-col :span="0.01">|</el-col>
-        <el-col :span="1"><div @click="change('/index')" style="cursor: pointer">主页</div></el-col>
-        <el-col :span="1" ><div @click="change('/health/attend')" style="cursor: pointer">打卡</div></el-col>
+      <el-row style="margin-bottom: 20px;">
+        <el-col :span="1" :offset="1" style="height: 60px">
+          <div class="center">
+            <img src="/favicon.ico" height="50" width="50">
+          </div>
+        </el-col>
+        <el-col :span="5" class="header-head">浙江大学疫情服务与管理系统</el-col>
+        <el-col :span="0.01" style="color: #fff">|</el-col>
+        <el-col :span="1">
+          <div class="header-box" @click="change('/index')">
+            <p class="header-font" style="cursor: pointer">主页</p>
+          </div>
+        </el-col>
+        <el-col :span="1" >
+          <div class="header-box" @click="change('/health/attend')">
+            <p class="header-font" style="cursor: pointer">打卡</p>
+          </div>
+        </el-col>
         <el-col :span="8" :offset="6">
-          <div><button @click="change('/login')">登录</button></div>
+          <div v-show="!isLogin" class="header-box" @click="change('/login')">
+            <p class="header-font" style="cursor: pointer">登录</p>
+          </div>
+          <div v-show="isLogin" class="user-box">
+            <el-row class="sub-row" type="flex" justify="center">
+              <el-col :span="3">
+                <el-dropdown style="width: 100%;">
+                  <div class="notice-box">
+                    <div style="display: flex; justify-content: center; align-items: center">
+                      <div class="notice">
+                        <i class="el-icon-bell" style="font-size: 20px; color: white"></i>
+                      </div>
+                    </div>
+                  </div>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>系统通知</el-dropdown-item>
+                    <el-dropdown-item>审批记录</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+              <el-col :span="8">
+                <el-dropdown style="width: 100%">
+                  <div class="user-info-box">
+                      <div class="user-img">
+                        <img src="/favicon.ico" height="40" width="40">
+                      </div>
+                      <div class="header-font">{{this.$store.state.username}}</div>
+                  </div>
+                  <el-dropdown-menu slot="dropdown">
+                    <el-dropdown-item>个人信息</el-dropdown-item>
+                    <el-dropdown-item>注销</el-dropdown-item>
+                  </el-dropdown-menu>
+                </el-dropdown>
+              </el-col>
+            </el-row>
+          </div>
         </el-col>
       </el-row>
     </el-header>
@@ -28,6 +76,16 @@
 //import HelloWorld from "@/components/HelloWorld";
 export default {
   name: 'Header',
+  data() {
+    return {
+      isLogin: !!localStorage.getItem('token'),
+    }
+  },
+  watch: {
+    '$route' (to, from) {
+      this.$router.go(0);
+    }
+  },
   methods: {
     change (path) {
       this.$router.push(path)
@@ -46,6 +104,14 @@ export default {
   width:100%;
 }
 
+.center {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .banner {
   width: 100%;
   text-align: center;
@@ -62,8 +128,75 @@ export default {
   position: relative;
 }
 
+.notice-box{
+  width: 100%;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.notice {
+  width:30px;
+  height: 30px;
+  background: gray;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+
+.header-box{
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.user-box{
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.user-info-box{
+  width: 100%;
+  height: 60px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+
+.header-head{
+  color: #fff;
+  font-size: 1.3rem;
+  font-family: KaiTi;
+}
+
+.header-font{
+  color: #fff;
+  font-size: 1rem;
+  font-family: 微软雅黑;
+}
+
+.sub-row{
+  height: 100%;
+  width: 100%;
+}
+
+.user-img{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .el-row {
-  margin-bottom: 20px;
   width: 100%;
 }
 .el-col {
@@ -71,7 +204,7 @@ export default {
 }
 
 .el-header, .el-footer {
-  background-color: #B3C0D1;
+  background-color: rgba(81,153,216,.8);
   color: #333;
   text-align: center;
   line-height: 60px;
