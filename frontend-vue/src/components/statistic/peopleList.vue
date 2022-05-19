c3503e9276c7c0b54c33cc26c050c3f04f1dc8ef
wuenzeboss <alex0132@163.com>
2022-05-19 20:31:20 +0800

    ADD: 完成未打卡统计后端交互

--- frontend-vue/src/components/statistic/peopleList.vue
+++ frontend-vue/src/components/statistic/peopleList.vue
@@ -8,37 +8,49 @@
           ref="multipleTable"
           :data="tableData.filter(data => !search || data.name.toLowerCase().includes(search.toLowerCase()))"
           height="500"
-
           tooltip-effect="dark"
           stripe
           @selection-change="handleSelectionChange"
-          style="width: 100%">
+          v-loading="loading"
+          style="width: 1050px"
+          >
         <el-table-column
             type="selection"
+            fixed
             width="44">
         </el-table-column>
         <el-table-column
             prop="id"
             label="学号"
+            fixed
             width="110">
         </el-table-column>
         <el-table-column
             prop="name"
             label="姓名"
+            fixed
+            width="90">
+        </el-table-column>
+        <el-table-column
+            prop="grade"
+            label="年级"
+            :filters="departmentList"
+            :filter-method="filterCol"
             width="90">
         </el-table-column>
         <el-table-column
             prop="department"
             label="学院"
+            :filters="departmentList"
+            :filter-method="filterCol"
             sortable
             width="200">
         </el-table-column>
         <el-table-column
             prop="major"
             label="专业"
-            :filters="[{text: '计算机科学与技术', value:'计算机科学与技术'}, {text:'软件工程', value: '软件工程'},
-                        {text:'信息安全', value: '信息安全'}]"
-            :filter-method="filterMajor"
+            :filters="majorList"
+            :filter-method="filterCol"
             sortable
             width="200">
         </el-table-column>
@@ -51,9 +63,10 @@
         <el-table-column
             prop="phone"
             label="手机号"
-            width="">
+            width="120">
         </el-table-column>
         <el-table-column
+            width="150"
             align="center">
           <template slot="header">
             <el-input
@@ -65,12 +78,12 @@
             <el-button
                 size="mini"
                 type="danger"
-                @click="handleDelete(scope.$index, scope.row)">发送通知</el-button>
+                @click="sendSingleNotice(scope.$index, scope.row)">发送通知</el-button>
           </template>
         </el-table-column>
       </el-table>
       <div style="margin-top: 20px">
-        <el-button @click="sendMessege()" type="danger">发送通知</el-button>
+        <el-button @click="sendMultiNotice()" type="danger">发送通知</el-button>
         <el-button @click="toggleSelection()" type="primary">取消选择</el-button>
 
       </div>
@@ -79,112 +92,24 @@
 </template>
 
 <script>
+import departmentJSON from './department.json'
+let departmentList = departmentJSON.departmentList
+
+import majorJSON from './major.json'
+let majorList = majorJSON.majorList
+
+import {unattendList, sendNotice } from "@/api/statistic";
+
 export default {
   name: "peopleList",
   data() {
     return {
+      loading: true,
+      departmentList: departmentList,
+      majorList: majorList,
       multipleSelection: [],
       search: '',
-      tableData: [{
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      }, {
-        id: '3190102222',
-        name: '王小虎',
-        department: '计算机科学与技术学院',
-        major: '计算机科学与技术',
-        class: '计科1901',
-        phone: '13888888888'
-      },
-      ]
+      tableData: []
     }
   },
   methods: {
@@ -200,10 +125,52 @@ export default {
     handleSelectionChange(val) {
       this.multipleSelection = val;
     },
-    filterMajor(value, row, column) {
+    filterCol(value, row, column) {
       const property = column['property'];
       return row[property] === value;
+    },
+    sendSingleNotice(index, row) {
+      var noticeList = [];
+      noticeList.push(row.id);
+      var send_data = {
+        noticeList: noticeList
+      }
+      sendNotice(send_data).then(res=>{
+        console.log(res)
+        this.$message.success('发送通知成功')
+      })
+    },
+    sendMultiNotice() {
+      this.$confirm('是否向所有选中的人发送消息?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        var noticeList = [];
+        this.multipleSelection.forEach(value => {
+          noticeList.push(value.id)
+        })
+        var send_data = {
+          noticeList: noticeList
+        }
+        sendNotice(send_data).then(res=>{
+          console.log(res)
+          this.$message.success('发送通知成功')
+        })
+      }).catch(() => {
+        this.$message({
+          type: 'info',
+          message: '已取消'
+        });
+      });
     }
+  },
+  async mounted() {
+    this.loading = true
+    unattendList().then(res=> {
+      this.tableData = res.data.unattendList;
+      this.loading = false;
+    })
   }
 }
 </script>
