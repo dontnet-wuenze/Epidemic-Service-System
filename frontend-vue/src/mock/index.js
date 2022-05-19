1a1ab3883169a1dd1835ec99e664f9b405910662
wuenzeboss <alex0132@163.com>
2022-05-19 18:33:45 +0800

    ADD: 完成健康打卡后端交互

--- frontend-vue/src/mock/index.js
+++ frontend-vue/src/mock/index.js
@@ -15,3 +15,28 @@ Mock.mock('http://localhost:8080/api/user/code', (req) => {
         code: 1
     }
 })
+
+Mock.mock('http://localhost:8080/api/user/attend', 'post', (req) => {
+    return {
+        name: '李白',
+        department: "软件工程",
+        grade : '本科生',
+        code: 1
+    }
+})
+
+Mock.mock('http://localhost:8080/api/user/attend', 'get', (req, res) => {
+    return {
+        code: 404,
+        status: true,
+        id: '3190103333',
+        name: '李白',
+        campus: '紫金港校区',
+        region: '天津市,市辖区,和平区',
+        onCampus: true,
+        codeStatus: 2,
+        fever: true,
+        control: true,
+        contact: true
+    }
+})
