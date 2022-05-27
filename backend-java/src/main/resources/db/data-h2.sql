93e9addf41f0ce44209b57129d576752440cd1f0
Wenbo Liu <Liuwb2001@gmail.comexit>
2022-05-27 17:01:34 +0800

    nothing

--- backend-java/src/main/resources/db/data-h2.sql
+++ backend-java/src/main/resources/db/data-h2.sql
@@ -6,9 +6,6 @@ INSERT INTO user (id, name, password, department, phoneNumber, email, authorizat
 INSERT INTO user (id, name, password, department, phoneNumber, email, authorization, healthyCode) VALUES
 (3190102475, 'Lwb', '1234567', '计算机科学与技术', '13333333334', 'test4@baomidou.com', 'student', 'green');
 
-INSERT INTO user (id, name, password, department, phoneNumber, email, authorization, healthyCode) VALUES
-(3190102297, 'Wez', '12345678', '计算机科学与技术', '13333333335', 'test3@baomidou.com', 'student', 'green');
-
 INSERT INTO monitor (id, name, password, department, phoneNumber, authorization) VALUES
 (111, 'Boss', '888888', '计算机科学与技术', '66666666666', 'monitor');
 
