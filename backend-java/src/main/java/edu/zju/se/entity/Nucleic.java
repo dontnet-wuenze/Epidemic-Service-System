55c17a82e347cc667fad60a04344ea3d1c9d6812
liborui.br <liborui.br@alibaba-inc.com>
2022-06-01 20:00:15 +0800

    fix field mapping

--- backend-java/src/main/java/edu/zju/se/entity/Nucleic.java
+++ backend-java/src/main/java/edu/zju/se/entity/Nucleic.java
@@ -1,11 +1,13 @@
 package edu.zju.se.entity;
 
-import java.io.Serializable;
-import java.time.LocalDate;
-
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
@@ -17,11 +19,12 @@ import lombok.Setter;
  * </p>
  *
  * @author Boris Li
- * @since 2022-05-29
+ * @since 2022-06-01
  */
 @Getter
 @Setter
 @Builder
+@TableName("nucleic")
 public class Nucleic implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -29,54 +32,63 @@ public class Nucleic implements Serializable {
     /**
      * 主键ID-表单ID
      */
-    @TableId(type = IdType.ASSIGN_ID)
     @JsonProperty("form_id")
+    @TableId(value = "form_id", type = IdType.ASSIGN_ID)
     private Long formId;
 
     /**
      * 姓名
      */
+    @TableField("name")
     private String name;
 
     /**
      * 学工号
      */
     @JsonProperty("staff_id")
+    @TableField("staff_id")
     private String staffId;
 
     /**
      * 预约日期
      */
-    private LocalDate appdate;
+    @TableField("app_date")
+    private String appDate;
 
     /**
      * 预约时间(1/2/3/4代表四个时间段)
      */
-    private Integer apptime;
+    @TableField("app_time")
+    private Integer appTime;
 
     /**
      * 预约检查点
      */
-    private String appaddress;
+    @TableField("app_address")
+    private String appAddress;
 
     /**
      * 电话号码
      */
+    @TableField("telenum")
     private String telenum;
 
     /**
      * 电子邮箱
      */
+    @TableField("email")
     private String email;
 
     /**
      * 所在校区
      */
+    @TableField("campus")
     private String campus;
 
     /**
      * 0阴性，1阳性，2未知
      */
+    @TableField("result")
     private Integer result;
 
 
